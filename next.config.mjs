efae2d54fc89e38a8edcaad3e9a2dae2d9a2d725
Dhinesh4668 <dhinesh4668@outlook.com>
2024-06-24 23:06:23 +0530

    issue #7 site not found

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-// next.config.js
-module.exports = {
-    // Use the 'exportPathMap' if needed
-    exportPathMap: async function (defaultPathMap) {
-        return {
-            '/': { page: '/' },
-            // Add other routes if necessary
-        }
-    },
-    trailingSlash: true, // Add trailing slash to directories
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
