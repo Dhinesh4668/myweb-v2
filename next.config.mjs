9570b3e6a4585ee7aaae8ef5c585fedd67dd71d8
Dhinesh4668 <dhinesh4668@outlook.com>
2024-06-24 23:12:22 +0530

    fixes: github site deploye site map fixes (#8)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Enable trailing slashes for GitHub Pages
     trailingSlash: true,
-    // Base path for GitHub Pages
     basePath: '/your-repo-name',
-    // Configuration for static export
     exportPathMap: async function (defaultPathMap) {
         return {
             '/': { page: '/' },
