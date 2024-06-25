a71a728171f83aca46767cc198c95e30a8f7635e
Dhinesh4668 <dhinesh4668@outlook.com>
2024-06-25 09:09:07 +0530

    -issue #16 -build issue fix -github deploye workflows fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    basePath: '/myweb-v2', // replace with your actual repo name
+};
 
 export default nextConfig;
