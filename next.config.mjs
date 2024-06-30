de1aee214f6a77cb7228147f77a9b7003da4d559
Dhinesh4668 <dhinesh4668@outlook.com>
2024-06-30 08:27:32 +0530

    base path fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
-    basePath: '/myweb-v2', // replace with your actual repo name
+    // basePath: '/myweb-v2', // replace with your actual repo name
 };
 
 export default nextConfig;
